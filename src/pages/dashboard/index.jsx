15844ad91936770b118077cb5fa0a4c4d124a8e7
mhdusop <mhduusop@gmail.com>
2023-12-22 16:19:17 +0700

    chore: init

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "./components/sidebar/Sidebar.jsx";
+import { Main } from "./components/main/Main.jsx";
+
+export const Dashboard = () => {
+   return (
+      <>
+         <Sidebar />
+         <Main />
+      </>
+   )
+}
