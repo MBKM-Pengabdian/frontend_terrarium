5cab45ccb159bfe0ddfdf8345bb8790f0915192e
mhdusop <mhduusop@gmail.com>
2023-12-26 12:16:43 +0700

    feat: add register page and consume api feat: add logout proccess

---
+++ src/pages/auth/logout/Logout.jsx
@@ -0,0 +1,18 @@
+import AuthService from "../../../services/auth.service";
+
+export const Logout = () => {
+   const authService = AuthService();
+
+   const handleLogout = () => {
+      authService.handleLogout();
+   };
+
+   return (
+      <>
+         <a className="btn btn-white m-0" onClick={handleLogout}>
+            <i className="fas fa-solid fa-arrow-right-from-bracket me-1"></i>
+            <span className="d-sm-inline d-none">Log Out</span>
+         </a>
+      </>
+   )
+}
