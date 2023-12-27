85161cb5df5fde15e7ff7e4112a98e4b3b1bd46d
mhdusop <mhduusop@gmail.com>
2023-12-27 14:57:44 +0700

    feat: add module main-page feat: setup page home feat: setup page product feat: setup page event feat: add component navbar and footer feat: add route per page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import { Dashboard } from "./pages/dashboard"
-import { Login } from "./pages/auth/login/Login"
-import { Register } from "./pages/auth/register/Register";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+import { Dashboard } from './pages/dashboard';
+import { Login } from './pages/auth/login/Login';
+import { Register } from './pages/auth/register/Register';
+import { Home, Product, Event } from './pages/main-page';
+import { Navbar } from './pages/main-page/components/navbar/Navbar';
 import PrivateRoute from './routes/PrivateRoutes';
+import { Footer } from './pages/main-page/components/footer/Footer';
 
-function App() {
+const AppWrapper = () => {
+  const location = useLocation();
+
+  // tambahkan path untuk module dashboard saja 
+  const excludePaths = ['/dashboard', '/login', '/register'];
+
+  const shouldExcludeNavbarFooter = excludePaths.includes(location.pathname);
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-        </Routes>
-      </Router>
+      {!shouldExcludeNavbarFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+      </Routes>
+      {!shouldExcludeNavbarFooter && <Footer />}
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/main-page/index.jsx
@@ -0,0 +1,5 @@
+import { Home } from "./pages/home_page";
+import { Event } from "./pages/event_page";
+import { Product } from "./pages/product_page"
+
+export { Home, Event, Product } 
