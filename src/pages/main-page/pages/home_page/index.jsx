91209e76106fb2e790f17baf51560d5207a30466
MhmmdSyahputra <putra.cokers@gmail.com>
2024-01-10 22:49:49 +0700

    style: redesain card produk in home

--- src/pages/main-page/pages/home_page/index.jsx
+++ src/pages/main-page/pages/home_page/index.jsx
@@ -1,4 +1,11 @@
-import { FaBitbucket, FaCartPlus, FaHandHoldingWater, FaSeedling, FaSpa, FaTree } from "react-icons/fa";
+import {
+  FaBitbucket,
+  FaCartPlus,
+  FaHandHoldingWater,
+  FaSeedling,
+  FaSpa,
+  FaTree,
+} from "react-icons/fa";
 
 export const Home = () => {
   return (
@@ -248,7 +255,9 @@ export const Home = () => {
                           <FaSpa className="display-4 text-primary" />
                         </div>
                         <div className="card-body">
-                          <h5 className="card-title h5">Produk Jadi Terrarium</h5>
+                          <h5 className="card-title h5">
+                            Produk Jadi Terrarium
+                          </h5>
                           <p
                             className="card-text"
                             style={{ fontSize: "0.85em" }}
@@ -267,10 +276,19 @@ export const Home = () => {
                   Produk yang Kami Tawarkan
                 </div>
                 <p>
-                  Dalam perjalanan kami menuju keberlanjutan dan kecantikan alam, kami dengan bangga mempersembahkan rangkaian produk unggulan kami yang mencerminkan keajaiban dunia tumbuhan. Setiap produk yang kami tawarkan tidak hanya sekadar barang, tetapi juga kisah cinta kami terhadap tanaman dan keberlanjutan.
+                  Dalam perjalanan kami menuju keberlanjutan dan kecantikan
+                  alam, kami dengan bangga mempersembahkan rangkaian produk
+                  unggulan kami yang mencerminkan keajaiban dunia tumbuhan.
+                  Setiap produk yang kami tawarkan tidak hanya sekadar barang,
+                  tetapi juga kisah cinta kami terhadap tanaman dan
+                  keberlanjutan.
                 </p>
                 <p>
-                  Kami memahami bahwa setiap tanaman memiliki cerita dan karakternya sendiri, dan itulah yang kami sajikan melalui produk-produk kami. Dari tanaman hidup yang segar hingga aksesoris terrarium yang elegan, kami berkomitmen untuk membawa keindahan hijau ke dalam kehidupan sehari-hari Anda.
+                  Kami memahami bahwa setiap tanaman memiliki cerita dan
+                  karakternya sendiri, dan itulah yang kami sajikan melalui
+                  produk-produk kami. Dari tanaman hidup yang segar hingga
+                  aksesoris terrarium yang elegan, kami berkomitmen untuk
+                  membawa keindahan hijau ke dalam kehidupan sehari-hari Anda.
                 </p>
               </div>
             </div>
@@ -386,74 +404,8 @@ export const Home = () => {
         </div>
       </div>
 
-      <div className="container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea nam
-        consectetur magni quos eaque eum ad earum voluptates aspernatur suscipit
-        quis corrupti libero qui, eligendi laborum veritatis cumque quas rerum!
-        Quis eos itaque debitis non dolor tenetur impedit repudiandae harum
-        consectetur voluptatibus? Obcaecati ratione expedita voluptatibus
-        nesciunt velit ipsum quas amet. Ipsam tempore perspiciatis temporibus
-        eligendi tempora illo dolores officia, odio facere, quis reiciendis ad!
-        Corrupti optio nostrum consectetur numquam cupiditate eligendi. Ipsum
-        reiciendis sapiente harum, placeat non quia recusandae eaque, autem
-        pariatur ipsam expedita fuga provident molestias perferendis dicta!
-        Eaque at sapiente veniam obcaecati voluptatem, voluptas architecto est
-        totam nemo beatae tempore ea debitis nostrum? Sit delectus maxime
-        consequatur fugit, consectetur nam, dolore consequuntur eligendi
-        adipisci ratione repellendus vero eius expedita quod quis aperiam saepe
-        veritatis quos distinctio fuga. Nemo provident, enim suscipit asperiores
-        temporibus nihil illo molestiae atque error harum officia eos libero
-        accusantium fugiat quis quas veritatis laudantium cum iusto
-        necessitatibus. Fugiat repudiandae dolores voluptatibus eaque quidem
-        nobis aliquid rem beatae, voluptatem fugit est sed soluta vel porro
-        quaerat? Dolorum quia vel dolores sunt. Quasi ullam quam assumenda
-        fugiat odit iste fuga officia eaque. Hic aliquam molestias ex rerum
-        reiciendis quis asperiores vel sequi accusantium. Laborum atque porro
-        quia distinctio similique, necessitatibus rerum ab eos maxime velit
-        dolore aspernatur minima voluptas exercitationem! Ut ipsa necessitatibus
-        praesentium laudantium maxime commodi velit enim at quae facere
-        repellendus veritatis animi earum modi debitis cum minus, sunt suscipit
-        cumque quos distinctio, quibusdam facilis. Repellendus illum iure, culpa
-        quia corrupti a nostrum excepturi, cumque error inventore, tenetur
-        aperiam. Odit ut quos saepe officia? Quae illo, repellat vero incidunt
-        exercitationem vel molestiae, soluta nulla voluptatibus veniam suscipit
-        voluptates aliquam earum. Ipsum odio neque maxime quis ipsa dolorum
-        possimus et accusantium illum? Velit libero esse aliquid dolore, placeat
-        id? Blanditiis ad quidem fugit. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Soluta, enim ullam eos tempore rerum eum sit
-        praesentium ab dolor corrupti quia explicabo ipsum sint, aut alias ipsam
-        expedita assumenda voluptas? Repudiandae ex dicta explicabo voluptatem
-        velit facilis voluptates quaerat officia sint, minus autem corrupti amet
-        doloremque architecto. Aspernatur sint optio voluptatum quia ullam
-        commodi beatae accusantium dicta suscipit magnam corrupti consequuntur
-        possimus pariatur tempora iure minima expedita harum, quod soluta error
-        velit sit unde, mollitia perferendis. Nam laudantium non natus dolores.
-        Illo eius quisquam consequatur et ipsam reprehenderit, incidunt
-        repudiandae eaque ratione accusantium, ex laudantium quasi facere.
-        Temporibus, facilis? Excepturi eveniet nostrum sunt dolore, perferendis
-        a? Expedita veritatis doloremque facere labore eum laboriosam rerum
-        repellendus eos qui voluptates ratione temporibus, nulla consectetur
-        reprehenderit nobis illo quas aperiam. Maiores possimus minima iure
-        soluta in atque repudiandae? Dolorum, iste voluptas at corporis
-        molestiae culpa a dolor cumque deserunt consequatur rerum expedita, ipsa
-        eius magnam esse itaque id natus, maiores odit. Eveniet illo perferendis
-        ipsam nostrum quod delectus maiores recusandae quos. Quis earum
-        recusandae odio ducimus ab placeat vel commodi modi aut, dolorem
-        doloremque in? Quis quaerat eius itaque voluptatibus optio. Cum
-        reiciendis alias quia repellendus expedita. Accusamus eligendi
-        voluptates rem! Nam, fugiat. Vero molestiae nisi odio, maxime numquam
-        similique eius enim voluptatum minima, et placeat? Soluta quisquam modi
-        quae consequatur dolorem perspiciatis officia est ullam debitis
-        explicabo? Maxime aliquid sapiente nesciunt sequi eveniet nobis
-        provident facilis sed? Molestias nemo fugit earum nobis minima sunt
-        deleniti debitis architecto voluptatibus unde quos ad labore rerum
-        consectetur reprehenderit itaque nesciunt explicabo hic animi a quod,
-        temporibus reiciendis aspernatur. Neque sapiente quae molestiae quisquam
-        voluptatem natus non corrupti error nesciunt reprehenderit itaque, ipsa
-        libero magnam ducimus ratione ipsum minus doloremque esse ipsam numquam
-        illo veritatis. Soluta ipsum minus officiis accusamus ipsam ex sit
-        voluptatum suscipit corrupti!
-        <div className="product-section-home my-5">
+      <div className="product-section-home my-5">
+        <div className="container">
           <div className="row mb-3">
             <div className="col">
               <div className="h4">Produk Kami</div>
@@ -461,30 +413,30 @@ export const Home = () => {
             <div className="col text-end">Selengkapnya</div>
           </div>
           <div className="row justify-content-between">
-            {["a", "b", "c"].map((data, index) => (
+            {["a", "b", "c", "d"].map((data, index) => (
               <div
                 key={index}
                 className="card px-0 mx-lg-3 rounded-3"
-                style={{ width: "25rem" }}
+                style={{ width: "18rem" }}
               >
                 <img
                   src="https://images.unsplash.com/photo-1635819335758-304866e30d39?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8"
                   className="card-img-top rounded-top"
-                  style={{ height: "300px", objectFit: "cover" }}
+                  style={{ height: "200px", objectFit: "cover" }}
                   alt="..."
                 />
-                <div className="card-body">
+                <div className="card-body pt-1">
                   <label htmlFor="">Kategori</label>
                   <h5
-                    className="card-title text-2line mb-2"
+                    className="card-title fs-6 text-2line"
                     style={{ minHeight: "50px" }}
                   >
                     Pot Bentuk Rumah Makan Adat Padang
                   </h5>
-                  <div className="text-end mb-3 fw-bold">Rp. 40.000</div>
+                  <div className="fw-bold">Rp. 40.000</div>
 
                   <div className="text-end">
-                    <button className="btn btn-success">
+                    <button className="btn btn-sm btn-success">
                       <FaCartPlus className="fs-5" />{" "}
                     </button>
                   </div>
@@ -493,7 +445,10 @@ export const Home = () => {
             ))}
           </div>
         </div>
-        <div className="event-section-home my-5">
+      </div>
+
+      <div className="event-section-home my-5">
+        <div className="container">
           <div className="row mb-3">
             <div className="col">
               <div className="h4">Event Terbaru</div>
@@ -573,91 +528,6 @@ export const Home = () => {
                 ))}
               </div>
             </div>
-
-            {/* <div
-              className="card px-0 mx-3 rounded-3"
-              style={{ width: "18rem" }}
-            >
-              <img
-                src="https://cdn.discordapp.com/attachments/1174741902415900742/1174741995751755776/event1.jpg?ex=65a94c3d&is=6596d73d&hm=c1553472ced677decf48a3f1fde03d47f2fcc1b868e0241fd334c128d3c0455a&"
-                className="card-img-top rounded-top"
-                style={{ height: "320px", objectFit: "cover" }}
-                alt="..."
-              />
-              <div className="card-body">
-                <h5
-                  className="card-title text-3line mb-4"
-                  style={{ minHeight: "75px" }}
-                >
-                  Workshop Tanaman Hidroponik
-                </h5>
-
-                <label className="text-end">2023-12-21</label>
-              </div>
-            </div>
-            <div
-              className="card px-0 mx-3 rounded-3"
-              style={{ width: "18rem" }}
-            >
-              <img
-                src="https://cdn.discordapp.com/attachments/1174741902415900742/1174741995453952050/event6.jpg?ex=65a94c3d&is=6596d73d&hm=1abad8839d80f9d454637eba5e6ead3459e651a54f035751e255e43414854e56&"
-                className="card-img-top rounded-top"
-                style={{ height: "320px", objectFit: "cover" }}
-                alt="..."
-              />
-              <div className="card-body">
-                <h5
-                  className="card-title text-3line mb-4"
-                  style={{ minHeight: "75px" }}
-                >
-                  Festival Pohon Rindang
-                </h5>
-
-                <label className="text-end">2023-12-21</label>
-              </div>
-            </div>
-            <div
-              className="card px-0 mx-3 rounded-3"
-              style={{ width: "18rem" }}
-            >
-              <img
-                src="https://cdn.discordapp.com/attachments/1174741902415900742/1174741996049534976/event2.jpg?ex=65a94c3d&is=6596d73d&hm=2fd2372b28476fc27aa65759378033c25190d883c9a7332d6aa3a1e0ffab912a&"
-                className="card-img-top rounded-top"
-                style={{ height: "320px", objectFit: "cover" }}
-                alt="..."
-              />
-              <div className="card-body">
-                <h5
-                  className="card-title text-3line mb-4"
-                  style={{ minHeight: "75px" }}
-                >
-                  Kelas Perawatan Tanaman Hias Tanaman Hias Tanaman Hias Tanaman
-                  Hias
-                </h5>
-
-                <label className="text-end">2023-12-21</label>
-              </div>
-            </div>
-            <div
-              className="card px-0 mx-3 rounded-3"
-              style={{ width: "18rem" }}
-            >
-              <img
-                src="https://cdn.discordapp.com/attachments/1174741902415900742/1191807387003261019/image.png?ex=65a6c81f&is=6594531f&hm=30644f92bb2a186059e110824657f3e13ca12c7d627158fd618a1e873d4adfeb&"
-                className="card-img-top rounded-top"
-                style={{ height: "320px", objectFit: "cover" }}
-                alt="..."
-              />
-              <div className="card-body">
-                <h5
-                  className="card-title text-3line mb-4"
-                  style={{ minHeight: "75px" }}
-                >
-                  Pameran Bunga Exotic
-                </h5>
-                <label className="text-end">2023-12-21</label>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
