b0addc926b2d55899bad0c15daa4d067213041de
MhmmdSyahputra <putra.cokers@gmail.com>
2024-01-22 22:49:04 +0700

    feat: redesain about page and refactor code

--- src/pages/main-page/components/navbar/Navbar.jsx
+++ src/pages/main-page/components/navbar/Navbar.jsx
@@ -1,83 +1,109 @@
 import { Link, useLocation } from "react-router-dom";
 import Logo from "./../../../../assets/img/logo.png";
+import { FaUser, FaUserCircle } from "react-icons/fa";
 
 export const Navbar = () => {
   const location = useLocation();
 
-  return (  
-   <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary text-dark bg-white">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          <img src={Logo} alt="Logo" width={70} />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item me-2">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/" ? "active" : ""
-                }`}
-                to="/"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item me-2">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/product" ? "active" : ""
-                }`}
-                to="/product"
-              >
-                Product
-              </Link>
-            </li>
-            <li className="nav-item me-2">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/event" ? "active" : ""
-                }`}
-                to="/event"
-              >
-                Event
-              </Link>
-            </li>
-            <li className="nav-item me-2">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/about" ? "active" : ""
-                }`}
-                to="/about"
-              >
-                About
-              </Link>
-            </li>
-            <li className="nav-item me-2">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/contact" ? "active" : ""
-                }`}
-                to="/contact"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary text-dark bg-white">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={Logo} alt="Logo" width={70} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item me-2">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/product" ? "active" : ""
+                  }`}
+                  to="/product"
+                >
+                  Product
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/event" ? "active" : ""
+                  }`}
+                  to="/event"
+                >
+                  Event
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/about" ? "active" : ""
+                  }`}
+                  to="/about"
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/contact" ? "active" : ""
+                  }`}
+                  to="/contact"
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="ourProductDropdown"
+                  data-bs-toggle="dropdown"
+                >
+                  <FaUserCircle className="fs-5 me-2"/>
+                  Putra
+                </a>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="ourProductDropdown"
+                >
+                  <a className="dropdown-item p-3" href="#ourProduct">
+                    Tiket Saya
+                  </a>
+                  <a className="dropdown-item p-3" href="#ourProduct">
+                    Pesanan Saya
+                  </a>
+                  <a className="dropdown-item p-3" href="#ourProduct">
+                    Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-   </>
+      </nav>
+    </>
   );
 };
--- src/pages/main-page/pages/about-page/index.jsx
+++ src/pages/main-page/pages/about-page/index.jsx
@@ -1,15 +1,35 @@
+import {
+  FaBitbucket,
+  FaHandHoldingWater,
+  FaSeedling,
+  FaSpa,
+} from "react-icons/fa";
 import img_about1 from "./../../../../assets/img/about-1.jpg";
 import img_about2 from "./../../../../assets/img/about-2.jpg";
 import img_banner1 from "./../../../../assets/img/banner-1.jpg";
+import img_forest from "./../../../../assets/img/forest.jpg";
 
 export const AboutPage = () => {
   return (
     <>
+      <BannerSection />
+      <SejarahSection />
+      <ServiceSection />
+      <VisiSection />
+    </>
+  );
+};
+
+export const BannerSection = () => {
+  return (
+    <>
       <div
-        className="container-fluid py-5 mb-5"
+        className="container-fluid mb-5 py-5"
         style={{
-          background:
-            'url("https://cdn.discordapp.com/attachments/1174741902415900742/1192021363226066995/Group_1_1.png?ex=65a78f67&is=65951a67&hm=9c775bc530131c19b9b3f664dd2d51330c4d99b6a77c9e046c1467f00dbd1faf&")',
+          background: `url(${img_banner1})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
         }}
       >
         <div className="container py-5">
@@ -17,11 +37,19 @@ export const AboutPage = () => {
           <div className="text-white mb-3 h4 text-center">Cacti Life</div>
         </div>
       </div>
-      <div className="about-section-home my-5 d-flex align-items-lg-center py-5">
+    </>
+  );
+};
+
+export const SejarahSection = () => {
+  return (
+    <>
+      <div className="d-flex align-items-lg-center mb-5 py-2">
         <div className="container z-index-1">
+          <span className="text-dark fw-bold fs-5">Cacti Life</span>
+          <div className="text-primary h2">Sejarah Perusahaan</div>
+          <div className="line-title mb-4"></div>
           <div className="row">
-            <span className="text-dark fw-bold fs-5">Cacti Life</span>
-            <div className="text-primary h2 mb-4">Sejarah Perusahaan</div>
             <div className="row">
               <div className="col-lg-7 mb-3 m-auto">
                 <p className="text-dark">
@@ -77,12 +105,17 @@ export const AboutPage = () => {
           </div>
         </div>
       </div>
+    </>
+  );
+};
 
+export const VisiSection = () => {
+  return (
+    <>
       <div
-        className="about-section-home my-5 d-flex align-items-lg-center py-5"
+        className="d-flex align-items-lg-center mb-5"
         style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1470058869958-2a77ade41c02?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGxhbnRzfGVufDB8fDB8fHww')",
+          backgroundImage: `url(${img_forest})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundAttachment: "fixed",
@@ -96,21 +129,19 @@ export const AboutPage = () => {
             zIndex: "0",
           }}
         ></div>
-        <div className="container z-index-1">
-          {/* <span className="text-light fw-bold fs-5">ABOUT US</span>
-          <div className="text-primary h2 mb-4">Welcome to Cacti Life</div> */}
-          <div className="row justify-content-around">
-            <div className="col-lg-5">
+        <div className="container-fluid z-index-1 py-5">
+          <div className="row justify-content-around py-4">
+            <div className="col-lg-4">
               <div className="row">
                 <div className="col-lg-12 mb-5">
                   <div className="h3 fs-4 mb-2 text-primary">Visi</div>
-                  <p className="text-light fs-6">
+                  <p className="text-light fs-5">
                     Mengembangkan usaha yang berbasis ekonomi kreatif.
                   </p>
                 </div>
                 <div className="col-lg-12">
                   <div className="h3 fs-4 mb-2 text-primary">Misi</div>
-                  <p className="text-light fs-6">
+                  <p className="text-light fs-5">
                     Memperkenalkan terrarium kepada masyarakat Kota Medan dan
                     sekitarnya, membentuk komunitas penggemar seni terrarium,
                     memberikan layanan edukasi kepada generasi milenial di Kota
@@ -119,16 +150,16 @@ export const AboutPage = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-4">
               <div className="col-lg-12 mb-5">
                 <div className="h3 fs-4 mb-2 text-primary">Tujuan</div>
-                <p className="text-light fs-6">
+                <p className="text-light fs-5">
                   Meraih keuntungan dari usaha berbasis ekonomi kreatif
                 </p>
               </div>
               <div className="col-lg-12">
                 <div className="h3 fs-4 mb-2 text-primary">Sasaran </div>
-                <p className="text-light fs-6">
+                <p className="text-light fs-5">
                   Memperluas pangsa pasar dalam pengembangan usaha berbasis
                   ekonomi kreatif khususnya pada bidang seni terrarium.
                 </p>
@@ -137,8 +168,70 @@ export const AboutPage = () => {
           </div>
         </div>
       </div>
+    </>
+  );
+};
 
-      <div className="container"></div>
+export const ServiceSection = () => {
+  const dataLayanan = [
+    {
+      icon: <FaSeedling className="display-4 text-primary " />,
+      title: "Tanaman",
+      subtitle:
+        "Some quick example text to build on the card title and make up the bulk",
+    },
+    {
+      icon: <FaBitbucket className="display-4 text-primary " />,
+      title: "Aksesoris Terrarium",
+      subtitle:
+        "Some quick example text to build on the card title and make up the bulk",
+    },
+    {
+      icon: <FaHandHoldingWater className="display-4 text-primary " />,
+      title: "Media Tanam",
+      subtitle:
+        "Some quick example text to build on the card title and make up the bulk",
+    },
+    {
+      icon: <FaSpa className="display-4 text-primary " />,
+      title: "Produk Jadi Terrarium",
+      subtitle:
+        "Some quick example text to build on the card title and make up the bulk",
+    },
+  ];
+  return (
+    <>
+      <div className="d-flex align-items-lg-center mb-5 py-2">
+        <div className="container z-index-1">
+          <div className="text-primary h2 text-center">Layanan Kami</div>
+          <div className="line-title m-auto mb-2"></div>
+          <p className="text-center mb-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, quo?
+          </p>
+          <div className="row justify-content-center">
+            {dataLayanan.map((data, index) => (
+              <div
+                key={index}
+                className="card px-0 mx-3 mb-5 rounded-3"
+                style={{ width: "15rem" }}
+              >
+                <div
+                  className="bg-dark d-flex align-items-center justify-content-center rounded-top"
+                  style={{ height: "130px" }}
+                >
+                  {data.icon}
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title h5">{data.title}</h5>
+                  <p className="card-text" style={{ fontSize: "0.85em" }}>
+                    {data.subtitle}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
